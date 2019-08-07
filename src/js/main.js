66e336693208bffeeb46f7cb1f865b1208f0665a
sasinskil <sasinskil@gmail.com>
2019-08-07 12:31:43 +0200

    added logic about glass counter and show/hide history-container

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,18 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
 
+const waterGlassWater = document.querySelector('.water-glass__water--js');
 const glassCounter = document.querySelector('.water-glass__counter--js');
 const addGlassButton = document.querySelector('.actions__add--js');
 const removeGlassButton = document.querySelector('.actions__delete--js');
@@ -22,11 +23,27 @@ const historyContainer = document.querySelector('.history-container--js');
 const hideHistoryButton = document.querySelector('.history-container__hide--button-js');
 const tableBody = document.querySelector('.table__body--js');
 
-historyButton.addEventListener('click', (e) => {
+glassCounter.innerHTML = 0;
+
+addGlassButton.addEventListener('click', () => {
+  glassCounter.innerHTML = parseInt(glassCounter.innerHTML) + 1;
+  waterGlassWater.classList.toggle('water-glass__water--animated');
+})
+
+removeGlassButton.addEventListener('click', () => {
+  if (parseInt(glassCounter.innerHTML) > 0) {
+    glassCounter.innerHTML = parseInt(glassCounter.innerHTML) - 1;
+    waterGlassWater.classList.toggle('water-glass__water--animated');
+  } else {
+    alert('Nie możesz już usunąć szklanki ;)');
+  }
+})
+
+historyButton.addEventListener('click', () => {
   historyContainer.classList.add('history-container__visible');
 })
 
-hideHistoryButton.addEventListener('click', (e) => {
+hideHistoryButton.addEventListener('click', () => {
   historyContainer.classList.remove('history-container__visible');
 })
 
