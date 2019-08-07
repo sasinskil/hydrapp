71cc3ccb17a500c9dafad74fae5fd48f2281eb53
sasinskil <sasinskil@gmail.com>
2019-08-07 12:10:16 +0200

    preparation of the querySelectiors for all the necessary elements

--- src/js/main.js
+++ src/js/main.js
@@ -14,9 +14,20 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// place your code below
+const glassCounter = document.querySelector('.water-glass__counter--js');
+const addGlassButton = document.querySelector('.actions__add--js');
+const removeGlassButton = document.querySelector('.actions__delete--js');
+const historyButton = document.querySelector('.history__button--js');
+const historyContainer = document.querySelector('.history-container--js');
+const hideHistoryButton = document.querySelector('.history-container__hide--button-js');
+const tableBody = document.querySelector('.table__body--js');
 
+historyButton.addEventListener('click', (e) => {
+  historyContainer.classList.add('history-container__visible');
+})
 
-console.log(`Hello world!`);
+hideHistoryButton.addEventListener('click', (e) => {
+  historyContainer.classList.remove('history-container__visible');
+})
 
 
